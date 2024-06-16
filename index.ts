2901c77fd366e4ceda2a748c1d92a6e05063bbc8
hrtrex <rafalgolik1@gmail.com>
2024-06-16 18:38:54 +0200

    aaaa

--- index.ts
+++ index.ts
@@ -3,20 +3,18 @@ import cors from "cors";
 import "dotenv/config";
 import * as RecipeAPI from "./recipe-api";
 import { PrismaClient } from "@prisma/client";
-// import path from "path";
+import path from "path";
 
 const app = express();
 const prismaClient = new PrismaClient();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 
 app.use(express.json());
 app.use(cors());
-// app.use(express.static(path.join(__dirname, './frontend/dist/index.html')));
-// app.get('*', (req, res) => {
-//   return res.sendFile(path.join(__dirname, './frontend/dist/index.html'));
-// });
+app.use(express.static(path.join(__dirname, './frontend/dist')));
+
 app.get('/', (req, res) => {
-  return res.json({"example": "value"});
+  res.sendFile(path.join(__dirname, './frontend/dist/index.html'));
 });
 
 app.get("/api/recipes/search", async (req, res) => {
